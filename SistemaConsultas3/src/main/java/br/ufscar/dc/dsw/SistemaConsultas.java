476f060a3aaade403eadd90f479866432fe9deac
amandapmn <40667726+amandapmn@users.noreply.github.com>
2021-06-30 02:39:32 -0300

    mais testes

--- SistemaConsultas3/src/main/java/br/ufscar/dc/dsw/SistemaConsultas.java
+++ SistemaConsultas3/src/main/java/br/ufscar/dc/dsw/SistemaConsultas.java
@@ -40,20 +40,36 @@ public class SistemaConsultas {
 			Date data1  = new SimpleDateFormat("dd/MM/yyyyHH:mm:ss").parse("22/08/202109:00:00");
 
 			Usuario usuario1 = new Usuario("admin@admin.com", encoder.encode("12345678"), "111.111.111-11", "Admin", "Admin", "ADMIN");
-			Cliente cliente1 = new Cliente("nathan.oliveirasc17@gmail.com", encoder.encode("12345678"), "222.222.222-22", "Matheus", "Freitas", "CLIENTE", "11 99999-9999", "Masculino", "11/11/1111");
+			Cliente cliente1 = new Cliente("nathan.oliveirasc17@gmail.com", encoder.encode("12345678"), "222.222.222-22", "Matheus", "Freitas", "CLIENTE", "11 99999-9999", "Masculino", "11/11/2005");
+			Cliente cliente2 = new Cliente("nathanael.silva@gmail.com", encoder.encode("12345678"), "444.444.444-44", "Nathanael", "Silva", "CLIENTE", "11 98888-9999", "Masculino", "09/12/2011");
 			Profissional profissional1 = new Profissional("pro1@pro.com", encoder.encode("12345678"), "333.333.333-33", "João", "Silva", "PROFISSIONAL", "Médico", "Medicina USP");
+			Profissional profissional2 = new Profissional("amandamanso7@gmail.com", encoder.encode("12345678"), "555.555.555-55", "Alice", "Peixoto", "PROFISSIONAL", "Cantora", "Música UFSCar");
 			Consulta consulta1 = new Consulta(data1, "dsfsfdssfd", cliente1, profissional1);
+			Consulta consulta2 = new Consulta(data1, "dsfsfASSSAS", cliente2, profissional1);
+			Consulta consulta3 = new Consulta(data1, "dsfssaasaSAS", cliente2, profissional2);
 			log.info("Salvando Usuario - Admin");
 			usuarioDAO.save(usuario1);
 
 			log.info("Salvando Cliente - Matheus Freitas");
 			clienteDAO.save(cliente1);
+			
+			log.info("Salvando Cliente - Nathanael Silva");
+			clienteDAO.save(cliente2);
 
 			log.info("Salvando Profissional - João Silva");
 			profissionalDAO.save(profissional1);
+			
+			log.info("Salvando Profissional - Alice Peixoto");
+			profissionalDAO.save(profissional2);
 
 			log.info("Salvando Consulta");
 			consultaDAO.save(consulta1);
+			
+			log.info("Salvando Consulta");
+			consultaDAO.save(consulta2);
+			
+			log.info("Salvando Consulta");
+			consultaDAO.save(consulta3);
 
 		};
 	}
